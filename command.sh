ddf58b001bbbe01b8bad526a865a57c3188b179e
wimjan <wimjan99@gmail.com>
2025-12-31 09:45:46 +0100

    Delete inject function from command.sh
    Removed the inject function that adds source command to .bashrc

--- command.sh
+++ command.sh
@@ -93,22 +93,6 @@ function hjelp() {
   echo "$function_list"
 }
 
-
-
-function inject() {
-local bashrc_file="$HOME/.bashrc"
- local bashfile_path="$(realpath "$0")"
-local source_command="source $bashfile_path"
-
-# Check if the source command is already in .bashrc
-if ! grep -Fxq "$source_command" "$bashrc_file"; then
- echo "$source_command" >> "$bashrc_file"
- echo "Added $bashfile_path to $bashrc_file"
- refrsh
-fi
-}
-inject
-
 function list_files() {
   local search_term="$1"
   local file_extension="$2"
