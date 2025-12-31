ec8ff20abf3598ac1bcd4624749e59196191972a
wimjan <wimjan99@gmail.com>
2025-12-31 09:46:07 +0100

    Remove inject function from work.sh
    Removed the inject function that adds source command to .bashrc.

--- work.sh
+++ work.sh
@@ -1,15 +1,1 @@
 alias devportal="cd /c/dev/"
-
-function inject() {
-local bashrc_file="$HOME/.bashrc"
- local bashfile_path="$(realpath "$0")"
-local source_command="source $bashfile_path"
-
-# Check if the source command is already in .bashrc
-if ! grep -Fxq "$source_command" "$bashrc_file"; then
- echo "$source_command" >> "$bashrc_file"
- echo "Added $bashfile_path to $bashrc_file"
- refrsh
-fi
-}
-inject
