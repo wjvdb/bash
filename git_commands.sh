120db6c110581cf526959567dde82c5d63687716
wimjan <wimjan99@gmail.com>
2025-12-31 09:50:13 +0100

    Add Git command aliases and unfuck function

---
+++ git_commands.sh
@@ -0,0 +1,22 @@
+alias gp='git push origin HEAD'
+alias gc='git commit' 
+alias gcm='git commit -m'
+alias gca='git add . && git commit -a -m'
+alias gac='git add . && git commit -a'
+alias gaa='git add .' 
+alias gau='git add --all' 
+alias ga='git add .' 
+alias derkje='git log --graph --decorate --pretty=oneline --abbrev-commit'
+alias gs='git status' 
+
+function unfuck() {
+  git fetch origin
+  current_branch=$(git symbolic-ref --short HEAD)
+  git reset --hard origin/$current_branch
+
+  read -p "Delete untracked files? (y/n): " delete_untracked
+  if [[ "$delete_untracked" =~ ^[Yy]$ ]]; then
+    git clean -fd
+    echo "Untracked files deleted."
+  fi
+}
