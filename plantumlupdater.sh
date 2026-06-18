2511f45844a1dd1b41d93cbc1f77a548a07a0dc6
Wimjan van den Berg <Wimjan.van.den.Berg@schiphol.nl>
2026-06-18 09:09:02 +0200

    patch

--- plantumlupdater.sh
+++ plantumlupdater.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# Download the latest PlantUML from git to /c/
+# Download the latest PlantUML JAR from git to /c/
 download_plantuml() {
-    local repo_url="https://github.com/plantuml/plantuml.git"
-    local target_dir="/c/plantuml"
+    local jar_url="https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar"
+    local target_file="/c/plantuml.jar"
     
-    if [ -d "$target_dir" ]; then
-        echo "Updating existing PlantUML repository..."
-        cd "$target_dir" || exit 1
-        git pull origin master
-    else
-        echo "Cloning PlantUML repository..."
-        git clone "$repo_url" "$target_dir"
-    fi
+    echo "Downloading PlantUML JAR..."
+    curl -L "$jar_url" -o "$target_file"
     
     if [ $? -eq 0 ]; then
-        echo "PlantUML download/update completed successfully at $target_dir"
+        echo "PlantUML JAR downloaded successfully to $target_file"
     else
-        echo "Error downloading/updating PlantUML"
+        echo "Error downloading PlantUML JAR"
         return 1
     fi
 }
